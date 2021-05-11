d17a2c49d2431061b2d48135d780b2ad675b3f3e
Your Name <you@example.com>
2021-05-11 17:52:28 +0200

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,16 @@
+<template lang="fr">
+  <div>
+    {{msg}}
+  </div>
+</template>
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+<style lang="">
+  
+</style>
---
+++ src/views/Home.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+import Vue from 'vue'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+
+Vue.use(VueAxios, axios)
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  },
+  mounted() {
+    Vue.axios.get(process.env.VUE_APP_URL+'cameras').then((response) => {
+      console.log(response.data)
+})
+  },
+}
+</script>
